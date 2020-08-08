a18b0bc4e7b3364d5a27b765085484c2733c6785
Johnny Ramirez <ramirezjohnny@icloud.com>
2020-08-08 00:13:57 -0400

    Change the whole function so that instead of iterating through each period in the schedule structure, it iterates through each period in the schedule times
    This elimantes all of the slopiness that we would have to eventually go through  and will allow for any schedul eto be used

--- server/src/services/input-to-schedule.ts
+++ server/src/services/input-to-schedule.ts
@@ -12,61 +12,40 @@ export const inputToSchedule = (schedule: ScheduleInput, scheduleStructure: Conf
     const { school, classes } = schedule;
     const classTimes = scheduleTimes[school];
 
-    return Object.fromEntries(Object.entries(scheduleStructure).map(([letter, blocks], index) => {
+    return Object.fromEntries(Object.entries(scheduleStructure).map((letter, index) => {
         const outputClasses: Period[] = [];
         const letterDay = Object.keys(scheduleStructure)[index];
-        let pastLunch = false;
-        blocks.forEach((dayPeriod, periodIndex) => {
+        classTimes.forEach((periodObject, periodIndex) => {
             let time = {
                 from: classTimes[periodIndex].from,
                 to: classTimes[periodIndex].to
             };
-
-            if (classTimes[periodIndex].period === SpecialPeriod.LUNCH) {
-                const period = classTimes[periodIndex].period;
-                outputClasses.push({period, time});
-                pastLunch = true;
-            }
-
-            if (pastLunch) {
-                time = {
-                    // to account for lunch, we need to offset the index by one
-                    from: classTimes[periodIndex + 1].from,
-                    to: classTimes[periodIndex + 1].to
-                };
-            }
-
-            const classForAssignedPeriod = classes.find(schoolClass => {
-                return schoolClass.period === dayPeriod && schoolClass.letterDays.includes(letterDay);;
-            });
-            
-            if (classForAssignedPeriod) {
+            if (periodObject.period === SpecialPeriod.LUNCH || periodObject.period  ===  SpecialPeriod.EXTRA_HELP) {
                 outputClasses.push({
-                    period: classForAssignedPeriod.period,
-                    name: classForAssignedPeriod.name,
-                    room: classForAssignedPeriod.room,
-                    teacher: classForAssignedPeriod.teacher,
-                    id: classes.indexOf(classForAssignedPeriod),
-                    time 
-                } as Class);
-            } else {
-                outputClasses.push({
-                    period: SpecialPeriod.FREE,
+                    period: classTimes[periodIndex].period, 
                     time
                 });
+            } else {
+                const classForAssignedPeriod = classes.find(schoolClass => {
+                    return schoolClass.period === periodObject.period && schoolClass.letterDays.includes(letterDay);;
+                });
+                if (classForAssignedPeriod) {
+                    outputClasses.push({
+                        period: classForAssignedPeriod.period,
+                        name: classForAssignedPeriod.name,
+                        room: classForAssignedPeriod.room,
+                        teacher: classForAssignedPeriod.teacher,
+                        id: classes.indexOf(classForAssignedPeriod),
+                        time 
+                    } as Class);
+                } else {
+                    outputClasses.push({
+                        period: SpecialPeriod.FREE,
+                        time
+                    });
+                }
             }
         });
-        // Activity Period
-        const extraHelpPeriod = classTimes.find(periodObject => periodObject.period === SpecialPeriod.EXTRA_HELP);
-        if (extraHelpPeriod) {
-            outputClasses.push({ 
-                period: classTimes[classTimes.length - 1].period, 
-                time: { 
-                    from: classTimes[classTimes.length - 1].from, 
-                    to: classTimes[classTimes.length - 1].to 
-                }
-            });
-        }
         return [letter, outputClasses];
     })) as Schedule;
 }
