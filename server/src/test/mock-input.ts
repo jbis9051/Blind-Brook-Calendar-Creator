697a39656b0ff99384c91a3f8da7a83bdded7062
jbis9051 <josh9051@gmail.com>
2020-08-14 14:06:42 -0400

    Update mock input

--- server/src/test/mock-input.ts
+++ server/src/test/mock-input.ts
@@ -1,11 +1,10 @@
-export default `A,C,D,F,G,H	1	H102	<COURSE#>	Honors Algebra II/Trigonometry	<SEC>	Seeger	<DATE>
-A,C,D,F,G,H	2	H206	<COURSE#>	Honors Computer Science	<SEC>	Kraeger	<DATE>
-A,B,D,E,F,G	3	H221	<COURSE#>	English 10	<SEC>	Colangelo	<DATE>
-A,B,D,E,F,G	4	H218	<COURSE#>	Honors Chemistry	<SEC>	Sugantino	<DATE>
-A,C,E	5	H218	<COURSE#>	Honors Chemistry Lab	<SEC>	Sugantino	<DATE>
-B,F,H	5	H108	<COURSE#>	Health	<SEC>	Carlone	<DATE>
-A,B,C,E,F,H	6	H219	<COURSE#>	Honors Spanish 3	<SEC>	Nacher	<DATE>
-B,D,H	7	H GYM	<COURSE#>	Tactical Sports	<SEC>	Beatty	<DATE>
-C,E,G	7	H210B	<COURSE#>	Digital Photography	<SEC>	Buonocore	<DATE>
-B,C,D,E,G,H	8	H205	<COURSE#>	Global History	<SEC>	Lewis	<DATE>
-A,B,C,D,F,G	9	H103	<COURSE#>	Community Service 10	<SEC>	Romm	<DATE>`;
+export default `A,C,D,F,G,H	1	H102	<COURSE#>	Class 1	<SEC>	Teacher 1	<DATE>
+A,C,D,F,G,H	2	H206	<COURSE#>	Example Class 2	<SEC>	Teacher 2	<DATE>
+A,B,D,E,F,G	3	H221	<COURSE#>	Class 3	<SEC>	3 Teacher	<DATE>
+A,B,D,E,F,G	4	H218	<COURSE#>	Example Class 4	<SEC>	Teacher 4	<DATE>
+A,C,E	5	H218	<COURSE#>	Class 5	<SEC>	Mr.Example	<DATE>
+B,F,H	5	H108	<COURSE#>	Example Class 6	<SEC>	Ms.Example	<DATE>
+A,B,C,E,F,H	6	H219	<COURSE#>	Class 7	<SEC>	Mrs.Example	<DATE>
+B,D,H	7	H GYM	<COURSE#>	Example Class 8	<SEC>	Example Exampler	<DATE>
+C,E,G	7	H210B	<COURSE#>	Class 9 Example	<SEC>	Mr.Example II	<DATE>
+B,C,D,E,G,H	8	H205	<COURSE#>	Calss 10 Example	<SEC>	Example 9	<DATE>`
